19c02bcf5ecd8ed458b53edc5626e01cd1a2d72b
llaghezza <llaghezza@veritran.com>
2021-05-11 23:56:49 -0300

    se agregan las cuadras y se le permite al player correr, y acercar y alejar el zoom

--- BOSS/Entities/Player.gd
+++ BOSS/Entities/Player.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 400
 var velocity:Vector2 = Vector2.ZERO
+onready var camera = $Camera2D
+export (int) var zoomInLimit = 9
+export (int) var zoomOutLimit = 1
 
 func get_input():
 	velocity = Vector2()
@@ -14,7 +17,17 @@ func get_input():
 	if Input.is_action_pressed('move_up'):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
+	if Input.is_action_pressed('run'):
+		velocity = velocity * 2
+	if Input.is_action_pressed("zoom-out") and camera.zoom.x <= zoomInLimit:
+		camera.zoom.x += 0.2
+		camera.zoom.y += 0.2
+	if Input.is_action_pressed("zoom-in") and camera.zoom.x >= zoomOutLimit:
+		camera.zoom.x -= 0.2
+		camera.zoom.y -= 0.2
+		
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	
