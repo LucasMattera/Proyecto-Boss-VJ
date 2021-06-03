cf6015bd5b2c7702e7be23b676a0e5ef3d0dd6c1
Gustavo Barreiro <barreirogustavo05@gmail.com>
2021-06-02 21:20:03 -0300

    aplicando shaders

---
+++ BOSS/ShaderTransitionout.gd
@@ -0,0 +1,10 @@
+extends ColorRect
+var value = 1.0
+
+
+func _physics_process(delta):
+	print(material.get("shader_param/progress")) 
+	material.set("shader_param/progress", value) 
+	value -= delta / 2
+	if value <= 0:
+		queue_free() 
